966245711b9aa72b678ccaed56227933719ea2c4
arisupriatna14 <arisupriatna65@gmail.com>
2018-02-27 11:59:31 +0700

    Update exercise-3.js - merapihkan indentasi

--- h8-p0-w2/selasa-w2/exercise-3.js
+++ h8-p0-w2/selasa-w2/exercise-3.js
@@ -1,29 +1,23 @@
 // [Exercise-3] - Mengenal penggunaan Conditional if-else dalam JavaScript
 var nama = "Ari";
-var peran = "Supriatna";
+var peran = "Bob";
 
 if (nama === " ") {
    console.log("Nama harus di isi!");
-} 
-   else if (peran === " ") {
-      console.log("Hallo " + nama + " pilih peranmu untuk memulai game!");
-   }
-      else if (peran === "Kevin") {
-         console.log("Selamat datang di Dunia Proxytia " + nama);
-         console.log("Hallo " + peran + " " + nama + ", Kamu dapat menyerang dengan senjatamu!");
-      }
-         else if (peran === "Stuart") {
-            console.log("Selamat datang di Dunia Proxytia " + nama);
-            console.log("Hallo " + peran + " " + nama + ", Kamu dapat menyerang dengan senjatamu!");
-         }
-            else if (peran === "Bob") {
-               console.log("Selamat datang di Dunia Proxytia " + nama);
-               console.log("Hallo " + peran + " " + nama + ", Kamu dapat menyerang dengan senjatamu!");
-            } 
-               else if (peran === "Gru") {
-                  console.log("Selamat datang di Dunia Proxytia " + nama);
-                  console.log("Hallo " + peran + " " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
-               } 
-                  else {
-                    console.log("Hallo " + nama + ", nama peranmu tidak tesedia. silahkan pilih peran yang tersedia!");
-                  }
+} else if (peran === " ") {
+   console.log("Hallo " + nama + " pilih peranmu untuk memulai game!");
+} else if (peran === "Kevin") {
+   console.log("Selamat datang di Dunia Proxytia " + nama);
+   console.log("Hallo " + peran + " " + nama + ", Kamu dapat menyerang dengan senjatamu!");
+} else if (peran === "Stuart") {
+   console.log("Selamat datang di Dunia Proxytia " + nama);
+   console.log("Hallo " + peran + " " + nama + ", Kamu dapat menyerang dengan senjatamu!");
+} else if (peran === "Bob") {
+   console.log("Selamat datang di Dunia Proxytia " + nama);
+   console.log("Hallo " + peran + " " + nama + ", Kamu dapat menyerang dengan senjatamu!");
+} else if (peran === "Gru") {
+   console.log("Selamat datang di Dunia Proxytia " + nama);
+   console.log("Hallo " + peran + " " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
+} else {
+  console.log("Hallo " + nama + ", nama peranmu tidak tesedia. silahkan pilih peran yang tersedia!");
+}
