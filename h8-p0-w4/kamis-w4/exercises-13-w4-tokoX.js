3f5f3326385f702f0a37aa54b65726dc761ac06b
Ari Supriatna <arisupriatna@MacBook-Air-Ari.local>
2018-03-22 17:05:14 +0700

    update exercises

--- h8-p0-w4/kamis-w4/exercises-13-w4-tokoX.js
+++ h8-p0-w4/kamis-w4/exercises-13-w4-tokoX.js
@@ -3,42 +3,55 @@ function countProfit(shoppers) {
                      ['Baju Zoro', 500000, 2],
                      ['Sweater Uniklooh', 175000, 1]
                    ];
-  
-  var hasilArr = []
 
-  //untuk mengecek apakah didalam array isinya kosong
+  var hasilAkhir = []
+
   if(shoppers.length === 0){
-    return hasilArr
+    return hasilAkhir
   }
 
-  //untuk listBarang
-  for(var i = 0; i < listBarang.length; i++){
+  //listBarang
+  for(let a = 0; a < listBarang.length; a++){
     var isiObj = {}
-    isiObj.product = listBarang[i][0]
+    isiObj.product = listBarang[a][0]
+    isiObj.pembeli = []
+
+    //untuk mengecek apakah ada pembeli yang membeli barang sama
+    var jmlAmount  = 0
+    for(let b = 0; b < shoppers.length; b++){
+      if(shoppers[b].product === isiObj.product && shoppers[b].amount <= listBarang[a][2]){
+        jmlAmount += shoppers[b].amount
+        isiObj.pembeli.push(shoppers[b].name)
+      }    
+    }
     
-    //untuk name shoppers
-    for(var j = 0; j < shoppers.length; j++){
-      isiObj.shoppers = []
-      if((shoppers[j].name === isiObj.product) && (shoppers[j+1].name === isiObj.product)){
-        isiObj.shoppers.push(shoppers[j].name)
-      }
-    }
+    //untuk mengecek apakah jumlah barang barang yang dibeli kurang dari stok yang tersedia
+    if(jmlAmount <= listBarang[a][2]){
 
-    if(shoppers[i].amount > listBarang[i][2]){
-      isiObj.lefOver =  listBarang[i][2]
+      isiObj.leftOver = listBarang[a][2] - jmlAmount
+      if(isiObj.product === listBarang[0][0]){
+        isiObj.totalProfit = jmlAmount * listBarang[0][1]
+      } else if(isiObj.product === listBarang[1][0]){
+        isiObj.totalProfit = jmlAmount * listBarang[1][1]
+      } else { 
+        isiObj.totalProfit = jmlAmount * listBarang[2][1]
+      } 
+
     } else {
-      isiObj.lefOver = listBarang[i][2] - shoppers[i].amount
+
+      isiObj.leftOver = listBarang[a][2]
+      isiObj.totalProfit = 0
+
     }
 
+    hasilAkhir.push(isiObj)
+  }
 
-
-    hasilArr.push(isiObj)
-  }  
-  return hasilArr
+  return hasilAkhir
 }
 
 // TEST CASES
-console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2}, {name: 'Vanessa', product: 'Baju Zoro', amount: 3}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 2}]));
+console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 3}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 2}]));
 //[ { product: 'Sepatu Stacattu',
 //   shoppers: [ 'Windi', 'Vanessa' ],
 //   leftOver: 5,
@@ -52,7 +65,7 @@ console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2},
 //   leftOver: 1,
 //   totalProfit: 0 } ]
 
-//console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 8}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 10}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 1}, {name: 'Devi', product: 'Baju Zoro', amount: 1}, {name: 'Lisa', product: 'Baju Zoro', amount: 1}]));
+console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 8}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 11}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 1}, {name: 'Devi', product: 'Baju Zoro', amount: 1}, {name: 'Lisa', product: 'Baju Zoro', amount: 1}]));
 // [ { product: 'Sepatu Stacattu',
 //     shoppers: [ 'Windi' ],
 //     leftOver: 2,
@@ -65,7 +78,7 @@ console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2},
 //     shoppers: [ 'Rani' ],
 //     leftOver: 0,
 //     totalProfit: 175000 } ]
-//console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
+console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
 // [ { product: 'Sepatu Stacattu',
 //     shoppers: [],
 //     leftOver: 10,
--- h8-p0-w5/selasa-w5/highestScore.js
+++ h8-p0-w5/selasa-w5/highestScore.js
@@ -1,89 +1,81 @@
 function highestScore (students) {
-    var isiObj = {}
-
-    for(var a = 0; a < students.length; a++){
-        
-        if(isiObj[students[a].class] === undefined){
-            isiObj[students[a].class] = students[a]
-        } else {
-          console.log(isiObj[students[a].class].score)
-            if(students[a].score > isiObj[students[a].class].score){
-                isiObj[students[a].class] = students[a]
-                
-            }
-        }
-        //console.log(students[a].score)
-        //console.log(isiObj[students[a].class].score + " " + students[a].score)
+  var isiObj = {}
+  //console.log(students.length)
+  for(let a = 0; a < students.length; a++){
+    if(isiObj[students[a].class] === undefined){
+      isiObj[students[a].class] = {name: students[a].name, score: students[a].score}
+    } else {
+      if(students[a].score > isiObj[students[a].class].score){
+        isiObj[students[a].class] = {name: students[a].name, score: students[a].score}
+      }
     }
-
-    //return isiObj
-    
-
+  }
+  return isiObj
 }
   
-  // TEST CASE
-  console.log(highestScore([
-    {
-      name: 'Dimitri',
-      score: 60,
-      class: 'foxes'
-    },
-    {
-      name: 'Alexei',
-      score: 55,
-      class: 'wolves'
-    },
-    {
-      name: 'Sergei',
-      score: 74,
-      class: 'foxes'
-    },
-    {
-      name: 'Anastasia',
-      score: 78,
-      class: 'wolves'
-    }
-  ]));
-  
-  // {
-  //   foxes: { name: 'Dimitri', score: 90 },
-  //   wolves: { name: 'Alexei', score: 85 }
-  // }
-  
-  
-//   console.log(highestScore([
-//     {
-//       name: 'Alexander',
-//       score: 100,
-//       class: 'foxes'
-//     },
-//     {
-//       name: 'Alisa',
-//       score: 76,
-//       class: 'wolves'
-//     },
-//     {
-//       name: 'Vladimir',
-//       score: 92,
-//       class: 'foxes'
-//     },
-//     {
-//       name: 'Albert',
-//       score: 71,
-//       class: 'wolves'
-//     },
-//     {
-//       name: 'Viktor',
-//       score: 80,
-//       class: 'tigers'
-//     }
-//   ]));
-  
-  // {
-  //   foxes: { name: 'Alexander', score: 100 },
-  //   wolves: { name: 'Alisa', score: 76 },
-  //   tigers: { name: 'Viktor', score: 80 }
-  // }
-  
-  
-  console.log(highestScore([])); //{}
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+
+console.log(highestScore([])); //{}
